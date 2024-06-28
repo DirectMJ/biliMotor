83ea7aa2c6789235eac37f1c15ccfe1e6dfdd072
DirectMJ <138039347+DirectMJ@users.noreply.github.com>
2024-06-28 08:07:24 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace _169_Pattaguan_MJ_act3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cb1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.rb1 = new System.Windows.Forms.RadioButton();
+            this.rb2 = new System.Windows.Forms.RadioButton();
+            this.tb1 = new System.Windows.Forms.TextBox();
+            this.lblmonthly = new System.Windows.Forms.Label();
+            this.tb4 = new System.Windows.Forms.TextBox();
+            this.tb3 = new System.Windows.Forms.TextBox();
+            this.tb2 = new System.Windows.Forms.TextBox();
+            this.lblinsurance = new System.Windows.Forms.Label();
+            this.lblreg = new System.Windows.Forms.Label();
+            this.lbltax = new System.Windows.Forms.Label();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.tb5 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cb1
+            // 
+            this.cb1.FormattingEnabled = true;
+            this.cb1.Location = new System.Drawing.Point(12, 31);
+            this.cb1.Name = "cb1";
+            this.cb1.Size = new System.Drawing.Size(118, 21);
+            this.cb1.TabIndex = 0;
+            this.cb1.SelectedIndexChanged += new System.EventHandler(this.cb1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(9, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "MOTORCYCLE";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(489, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "PAYMENT METHOD";
+            // 
+            // rb1
+            // 
+            this.rb1.AutoSize = true;
+            this.rb1.Location = new System.Drawing.Point(492, 35);
+            this.rb1.Name = "rb1";
+            this.rb1.Size = new System.Drawing.Size(75, 17);
+            this.rb1.TabIndex = 4;
+            this.rb1.Text = "Installment";
+            this.rb1.UseVisualStyleBackColor = true;
+            this.rb1.CheckedChanged += new System.EventHandler(this.rb1_CheckedChanged);
+            // 
+            // rb2
+            // 
+            this.rb2.AutoSize = true;
+            this.rb2.Location = new System.Drawing.Point(492, 58);
+            this.rb2.Name = "rb2";
+            this.rb2.Size = new System.Drawing.Size(49, 17);
+            this.rb2.TabIndex = 5;
+            this.rb2.Text = "Cash";
+            this.rb2.UseVisualStyleBackColor = true;
+            this.rb2.CheckedChanged += new System.EventHandler(this.rb2_CheckedChanged);
+            // 
+            // tb1
+            // 
+            this.tb1.Location = new System.Drawing.Point(29, 88);
+            this.tb1.Name = "tb1";
+            this.tb1.Size = new System.Drawing.Size(100, 20);
+            this.tb1.TabIndex = 6;
+            // 
+            // lblmonthly
+            // 
+            this.lblmonthly.AutoSize = true;
+            this.lblmonthly.Location = new System.Drawing.Point(26, 72);
+            this.lblmonthly.Name = "lblmonthly";
+            this.lblmonthly.Size = new System.Drawing.Size(86, 13);
+            this.lblmonthly.TabIndex = 7;
+            this.lblmonthly.Text = "mothly - payment";
+            // 
+            // tb4
+            // 
+            this.tb4.Location = new System.Drawing.Point(29, 241);
+            this.tb4.Name = "tb4";
+            this.tb4.Size = new System.Drawing.Size(100, 20);
+            this.tb4.TabIndex = 8;
+            this.tb4.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // tb3
+            // 
+            this.tb3.Location = new System.Drawing.Point(29, 191);
+            this.tb3.Name = "tb3";
+            this.tb3.Size = new System.Drawing.Size(100, 20);
+            this.tb3.TabIndex = 9;
+            // 
+            // tb2
+            // 
+            this.tb2.Location = new System.Drawing.Point(29, 136);
+            this.tb2.Name = "tb2";
+            this.tb2.Size = new System.Drawing.Size(100, 20);
+            this.tb2.TabIndex = 10;
+            // 
+            // lblinsurance
+            // 
+            this.lblinsurance.AutoSize = true;
+            this.lblinsurance.Location = new System.Drawing.Point(26, 120);
+            this.lblinsurance.Name = "lblinsurance";
+            this.lblinsurance.Size = new System.Drawing.Size(53, 13);
+            this.lblinsurance.TabIndex = 11;
+            this.lblinsurance.Text = "insurance";
+            // 
+            // lblreg
+            // 
+            this.lblreg.AutoSize = true;
+            this.lblreg.Location = new System.Drawing.Point(26, 175);
+            this.lblreg.Name = "lblreg";
+            this.lblreg.Size = new System.Drawing.Size(58, 13);
+            this.lblreg.TabIndex = 12;
+            this.lblreg.Text = "registration";
+            // 
+            // lbltax
+            // 
+            this.lbltax.AutoSize = true;
+            this.lbltax.Location = new System.Drawing.Point(26, 225);
+            this.lbltax.Name = "lbltax";
+            this.lbltax.Size = new System.Drawing.Size(21, 13);
+            this.lbltax.TabIndex = 13;
+            this.lbltax.Text = "tax";
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(481, 211);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(86, 40);
+            this.btn1.TabIndex = 14;
+            this.btn1.Text = "SAVE";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(584, 211);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(77, 40);
+            this.btn2.TabIndex = 15;
+            this.btn2.Text = "CANCEL";
+            this.btn2.UseVisualStyleBackColor = true;
+            // 
+            // tb5
+            // 
+            this.tb5.Enabled = false;
+            this.tb5.Location = new System.Drawing.Point(29, 282);
+            this.tb5.Name = "tb5";
+            this.tb5.Size = new System.Drawing.Size(100, 20);
+            this.tb5.TabIndex = 16;
+            this.tb5.TextChanged += new System.EventHandler(this.tb5_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 266);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 13);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "downpayment";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(715, 327);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tb5);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.lbltax);
+            this.Controls.Add(this.lblreg);
+            this.Controls.Add(this.lblinsurance);
+            this.Controls.Add(this.tb2);
+            this.Controls.Add(this.tb3);
+            this.Controls.Add(this.tb4);
+            this.Controls.Add(this.lblmonthly);
+            this.Controls.Add(this.tb1);
+            this.Controls.Add(this.rb2);
+            this.Controls.Add(this.rb1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cb1);
+            this.Name = "Form1";
+            this.Text = "sni";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cb1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton rb1;
+        private System.Windows.Forms.RadioButton rb2;
+        private System.Windows.Forms.TextBox tb1;
+        private System.Windows.Forms.Label lblmonthly;
+        private System.Windows.Forms.TextBox tb4;
+        private System.Windows.Forms.TextBox tb3;
+        private System.Windows.Forms.TextBox tb2;
+        private System.Windows.Forms.Label lblinsurance;
+        private System.Windows.Forms.Label lblreg;
+        private System.Windows.Forms.Label lbltax;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.TextBox tb5;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _169_Pattaguan_MJ_act3
+{
+    public partial class Form1 : Form
+    {
+        double advInstallment = 274840.00;
+        double pcxInstallment = 250400.00;
+        double clickInstallment = 153800.00;
+        double advCash = 168800.00;
+        double pcxCash = 151800.00;
+        double clickCash = 82800.00;
+        double insurance = 4500.00;
+        double registration = 3100.00;
+        int months = 36;
+        public Form1()
+        {
+            
+            InitializeComponent();
+
+            cb1.Items.Add("ADV");
+            cb1.Items.Add("CLICK");
+            cb1.Items.Add("PCX");
+            cb1.SelectedIndex = 0;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rb1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rb1.Checked == true)
+            {
+                tb5.Enabled = true;
+            }
+        }
+
+        private void rb2_CheckedChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            double motorcyclePrice = 0;
+            double downPayment = 0;
+            double monthlyInstallment = 0;
+            double tax = 0;
+            double totalPayment = 0;
+
+            if (rb1.Checked)
+            {
+
+                if (cb1.SelectedItem.ToString() == "ADV")
+                {
+                    motorcyclePrice = advInstallment;
+                }
+                else if (cb1.SelectedItem.ToString() == "PCX")
+                {
+                    motorcyclePrice = pcxInstallment;
+                }
+                else if (cb1.SelectedItem.ToString() == "CLICK")
+                {
+                    motorcyclePrice = clickInstallment;
+
+                }
+
+
+                if (!double.TryParse(tb5.Text, out downPayment))
+                {
+                    MessageBox.Show("Please enter a valid downpayment amount.");
+                    return;
+                }
+                double remainingAmount = motorcyclePrice - downPayment;
+
+
+                monthlyInstallment = remainingAmount / months;
+
+
+                totalPayment = remainingAmount + (monthlyInstallment * months);
+
+                tax = motorcyclePrice * 0.11;
+
+
+                tb1.Text = monthlyInstallment.ToString("0.00");
+                tb2.Text = insurance.ToString("0.00");
+                tb3.Text = registration.ToString("0.00");
+                tb4.Text = tax.ToString("0.00");
+            }
+            else if (rb2.Checked)
+            {
+
+                if (cb1.SelectedItem.ToString() == "ADV")
+                {
+                    motorcyclePrice = advCash;
+                }
+                else if (cb1.SelectedItem.ToString() == "PCX")
+                {
+                    motorcyclePrice = pcxCash;
+                }
+                else if (cb1.SelectedItem.ToString() == "CLICK")
+                {
+                    motorcyclePrice = clickCash;
+                }
+
+
+                tax = motorcyclePrice * 0.03;
+
+
+                tb1.Text = "Fully Paid";
+                tb4.Text = tax.ToString("0.00");
+                tb2.Text = insurance.ToString("0.00");
+                tb3.Text = registration.ToString("0.00");
+            }
+        }
+
+        
+
+        private void cb1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tb5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
